515d41443f4da00da55b084a1e178c38c1046db6
xiaoping.xu <xiaoping.xu@weimob.com>
2019-04-22 19:17:37 +0800

    fdf

--- app/pages/scope.controller.js
+++ app/pages/scope.controller.js
@@ -1,13 +1,97 @@
 (function(){
     angular.
-    module('phonecatApp').controller('ScopeController',ScopeController)
-
+    module('phonecatApp')
+    .controller('ScopeController',ScopeController)
+    .controller('ScopeController2',ScopeController2)
+    .controller('ListCtrl',ListCtrl)
+    .controller('EventController',EventController)
+    .controller('FilterController',FilterController)
+    .filter('revert', reverse)
+    .controller('MyController',MyController)
+    .controller('FormController',FormController)
+    
 
     ScopeController.$inject = ['$scope', '$state'];
     function ScopeController($scope, $state) {
         $scope.username = '';
         $scope.sayHello = function(){
-            console.log(`你好，${$scope.username}`)
+            $scope.greeting = 'Hello ' + $scope.username + '!';
         }
     }
+
+    ScopeController2.$inject = ['$scope', '$state','$rootScope'];
+    function ScopeController2($scope, $state,$rootScope) {
+        $scope.name = 'World';
+        $rootScope.department = 'Angular';
+    }
+
+
+       
+    function ListCtrl($scope) {
+    $scope.names = ['Igor', 'Misko', 'Vojta'];
+    }
+
+    function EventController($scope){
+        $scope.count = 0;
+        $scope.$on('MyEvent', function() {
+            $scope.count++;
+        });
+    }
+
+    FilterController.$inject = ['$scope','filterFilter']
+    function FilterController($scope,filterFilter) {
+        // this 指向controller 实例
+        $scope.array = [
+          {name: 'asnowwolf'},
+          {name: 'why520crazy'},
+          {name: 'joe'},
+          {name: 'ckken'},
+          {name: 'lightma'},
+          {name: 'FrankyYang'}
+        ];
+        $scope.filteredArray = filterFilter($scope.array, 'a');
+      }
+
+
+      // 注册过滤器
+       function reverse() {
+        return function(input, uppercase) {
+          input = input || '';
+          var out = '';
+          for (var i = 0; i < input.length; i++) {
+            out = input.charAt(i) + out;
+          }
+          // conditional based on optional argument
+          if (uppercase) {
+            out = out.toUpperCase();
+          }
+          return out;
+        };
+      }
+      MyController.$inject = ['$scope', 'revertFilter']
+      function MyController($scope, revertFilter) {
+        $scope.greeting = 'hello';
+        $scope.filteredGreeting = revertFilter($scope.greeting);
+      }
+
+
+
+      // 指令
+      function FormController($scope) {
+        $scope.master = {};
+       
+        $scope.update = function(user) {
+          $scope.master = angular.copy(user);
+        };
+       
+        $scope.reset = function() {
+          $scope.user = angular.copy($scope.master);
+        };
+       
+        $scope.isUnchanged = function(user) {
+            return angular.equals(user, $scope.master);
+          };
+         
+        $scope.reset();
+      }
 })()
--- app/router/scope.js
+++ app/router/scope.js
@@ -12,17 +12,64 @@
                             templateUrl: '/pages/scope.html',
                         }
                     },
-                    // resolve: ['$ocLazyLoad', function($ocLazyLoad) {
-                    //     return $ocLazyLoad.load('scopeModule'); // 按需加载目标 js file
-                    // }]
-                    // resolve: {
-                    //     deps: ['$ocLazyLoad', function ($ocLazyLoad) {
-                    //         return $ocLazyLoad.load([
-                    //             'couponOrderModule',
-                    //         ]).then(function () {
-                    //         });
-                    //     }]
-                    // }
+                })
+                .state('home.child.scope1', {
+                    url: '/scope1',
+                    // template:"三级路由"
+                    views: {
+                        "@home.child": {
+                            controller: 'ScopeController2',
+                            templateUrl: '/pages/scope1.html',
+                        }
+                    },
+                })
+                .state('home.child.scope2', {
+                    url: '/scope2',
+                    // template:"三级路由"
+                    views: {
+                        "@home.child": {
+                            controller: 'EventController',
+                            templateUrl: '/pages/scope2.html',
+                        }
+                    },
+                })
+                .state('home.child.scope3', {
+                    url: '/scope3',
+                    // template:"三级路由"
+                    views: {
+                        "@home.child": {
+                            controller: 'FilterController',
+                            templateUrl: '/pages/scope3.html',
+                        }
+                    },
+                })
+                .state('home.child.filter', {
+                    url: '/filter',
+                    // template:"三级路由"
+                    views: {
+                        "@home.child": {
+                            // controller: 'FilterController',
+                            templateUrl: '/pages/filter.html',
+                        }
+                    },
+                })
+                .state('home.child.form', {
+                    url: '/form',
+                    views: {
+                        "@home.child": {
+                            controller: 'FormController',
+                            templateUrl: '/pages/form.html',
+                        }
+                    },
+                })
+                .state('home.child.form2', {
+                    url: '/form2',
+                    views: {
+                        "@home.child": {
+                            controller: 'FormController',
+                            templateUrl: '/pages/form2.html',
+                        }
+                    },
                 })
         });
 })();
